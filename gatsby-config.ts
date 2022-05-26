a177df5f2c7cb0eb8d94c2bbea54ebfdf480c7b0
unknown <lsjayawardana95@gmail.com>
2022-05-27 01:25:10 +0530

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `coachello-landing-page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
