ea6e61b38ae6ff08ba798fb5f647a4ce55867d4b
Lasitha <lsjayawardana95@gmail.com>
2022-06-13 18:13:20 +0530

    netlify build fixed

--- src/utils/prismic.ts
+++ src/utils/prismic.ts
@@ -1,9 +1,11 @@
 import * as prismic from '@prismicio/client'
+import fetch from 'node-fetch'
 
 // Fill in your repository name
 export const repositoryName = 'coachello'
 
 export const client = prismic.createClient(repositoryName, {
+    fetch,
     // If your repository is private, add an access token
     accessToken: 'MC5ZcUx2M1JJQUFDUUFTbHpw.77-9CC8zRxdMZE7vv73vv70wYu-_vSQN77-977-977-9InEaUj1M77-977-9Ye-_ve-_vWFz',
 
@@ -13,7 +15,5 @@ export const client = prismic.createClient(repositoryName, {
     //
     // If you are not using a router in your project, you can change this
     // to an empty array or remove the option entirely.
-    routes: [
-
-    ],
+    routes: [],
 })
