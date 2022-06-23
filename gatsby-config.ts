2be9d69c1130693970ccfe05874909211e00432d
Lasitha <lsjayawardana95@gmail.com>
2022-06-24 01:36:06 +0530

    fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,7 +7,7 @@ const config: GatsbyConfig = {
         description: "Online Professional Coaching Platform",
         author: `@coachello`,
         url: `https://coachello.io`,
-        image: "src/assets/img/SEO.jpg", // Path to the image placed in the 'static' folder, in the project's root directory.
+        image: "https://ibb.co/DRT2z2t", // Path to the image placed in the 'static' folder, in the project's root directory.
     },
     plugins: [
         // used to generate rewrites for client only paths
